83f7e2c1bf5ef9d2aba6cf5818e0166909a88252
calebjanderson <caleb.anderson130@gmail.com>
2017-11-07 22:43:17 -0600

    Start with testMessages rendering

--- react-client/components/Chat.js
+++ react-client/components/Chat.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Message from './Message'
 
-var messages = [{id: 3, username: 'Alice', message: 'Wow REact is awesome!'}, {id: 4, username: 'Bob', message: 'Do people actually chat here?'}, {id: 5, username: 'Carly', message: 'Im a property of the 3rd object in an array of messages'}]
+var testMessages = [{id: 'tester1', username: 'Alice', message: 'Wow REact is awesome!'}, {id: 'tester2', username: 'Bob', message: 'Do people actually chat here?'}, {id: 'tester3', username: 'Carly', message: 'Im a property of the 3rd object in an array of messages'}]
 
 
 
-var Chat = () => {
+var Chat = ({ messages }) => {
   return (
     <div style={{ 'border': 'solid 5px blue', 'padding': '10px' }}>
       <h4>Chatroom</h4>
       <ul>
+        {
+          testMessages.map((m) => <Message key={m.id} username={m.username} message={m.message} />)
+        }
         {
           messages.map((m) => <Message key={m.id} username={m.username} message={m.message} />)
         }
